4f1348fab7bd788c74e45d41b7e59345aad3c48c
Gadi Piperno Corcos <gadi@skyriseltd.com>
2021-04-04 14:31:40 +0200

    :sparkles: Moves grouped by turn in MoveList component

--- src/components/MoveList.tsx
+++ src/components/MoveList.tsx
@@ -1,32 +1,54 @@
 import React from "react"
 import { Box, List, Text, Avatar } from 'grommet'
 
-import { Match } from "../match"
+import { Entry, Match, PlayerTurn } from "../match"
 
-const MoveList = ({ match, onSelected }: { match: Match, onSelected: Function }) => {
+const MoveList = ({ match, onSelected }: { match: Match, onSelected?: Function }) => {
     if (!match) return null
 
-    let entries = match.getEntries()
+    //let entries = match.getEntries()
+    let turns = match.getTurns()
 
-    const [selected, setSelected] = React.useState<number>(entries.indexOf(match.getCurrentEntry()));
+    const [selectedEntry, setSelectedEntry] = React.useState<Entry>(match.getCurrentEntry());
     //setSelected(match?.getCurrentEntry().id)
+
+    function selectEntry(entry: Entry){
+        match.selectEntry(entry.id)
+        setSelectedEntry(entry)
+        onSelected()
+    }
+
+    return <List
+        pad="none"
+        border="bottom"
+        data={turns}
+        //{itemProps={selected >= 0 ? { [selected]: { background: 'focus' } } : undefined}
+        primaryKey={turn => <Turn match={match} turn={turn} selectedEntry={selectedEntry} onSelected={selectEntry}/>}
+    />
+}
+
+const Turn = ({ match, turn, selectedEntry, onSelected }: { match: Match, turn: PlayerTurn, selectedEntry?: Entry, onSelected: Function }) => {
+    let selected = turn.entries.indexOf(selectedEntry)
+    let list = selected >= 0 ? turn.entries : [turn.entries[0]]
 
     return <List
         border="bottom"
-        data={entries}
+        data={list}
         itemProps={selected >= 0 ? { [selected]: { background: 'focus' } } : undefined}
-        primaryKey={({ turn, moveNumber }) => (<Box direction="row">
-            <Avatar size="small" background={match.getPlayerColorHex(turn.playerId)} margin={{ right: 'small' }}>
-                P{turn.playerId + 1}
-            </Avatar>
-            <Text weight={moveNumber > 0 ? 'normal' : 'bold'}>{moveNumber > 0 ? 'Move ' + moveNumber : 'Turn ' + turn.turnNumber}</Text>
-        </Box>)}
+        primaryKey={entry => <Move match={match} entry={entry} />}
         onClickItem={event => {
-            match.selectEntry(event.item.id)
-            setSelected(event.index)
-            onSelected()
+            onSelected(event.item)
         }}
     />
+}
+
+const Move = ({ match, entry: { moveNumber, turn } }: { match: Match, entry: Entry }) => {
+    return <Box direction="row">
+        <Avatar size="small" background={match.getPlayerColorHex(turn.playerId)} margin={{ right: 'small' }}>
+            P{turn.playerId + 1}
+        </Avatar>
+        <Text weight={moveNumber > 0 ? 'normal' : 'bold'}>{moveNumber > 0 ? 'Move ' + moveNumber : 'Turn ' + turn.turnNumber}</Text>
+    </Box>
 }
 
 export default MoveList
